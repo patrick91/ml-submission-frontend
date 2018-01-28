734ab47963f54439db026a91977d67c9625e00c1
Patrick Arminio <patrick.arminio@gmail.com>
2018-01-28 13:56:04 +0000

    Initial boilerplate

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
+import Grid from "material-ui/Grid";
+
+import { Switch, Route } from "react-router";
+
+import { BrowserRouter } from "react-router-dom";
+
+import Header from "./components/header";
+
+const App = () => (
+  <BrowserRouter>
+    <MuiThemeProvider>
+      <Header />
+      <Grid
+        style={{ margin: "40px auto", width: "90%", maxWidth: "1600px" }}
+        container
+        spacing={0}
+      >
+        <Grid item xs={12}>
+          <Switch>
+            <Route exact path="/" component={() => <h1>Home</h1>} />
+            <Route component={() => <h1>Not Found</h1>} />
+          </Switch>
+        </Grid>
+      </Grid>
+    </MuiThemeProvider>
+  </BrowserRouter>
+);
+
+export default App;
